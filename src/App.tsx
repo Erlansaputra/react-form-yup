1ca944435bb7871e3bc1a7a809362602272fb5eb
Erlansaputra <Erlansaputra2201@gmail.com>
2021-01-09 17:19:39 +0700

    Bismillah

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useForm } from "react-hook-form";
+import * as yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup'
 
-function App() {
+function later(delay: number) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, delay);
+  });
+}
+
+type FormInputs = {
+  fullName: string;
+};
+
+
+const validationSchema = yup.object().shape({
+  fullName: yup.string().required()
+})
+
+function Form() {
+  const {
+    register,
+    handleSubmit,
+    errors,
+    formState,
+    reset
+  } = useForm<FormInputs>({
+    mode:'onSubmit',
+    resolver: yupResolver(validationSchema)
+  });
+  const onSubmit = async (data: FormInputs) => {
+    console.log(data);
+    await later(3000);
+    alert("Thank you for Register");
+    reset();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h1> Create an Account </h1>
+      <form>
+        <label> Fullname * </label>
+        <input
+          type="text"
+          name="fullName"
+          ref={register()}
+        />
+        {errors.fullName && (
+          <p className="error"> {errors.fullName.message} </p>
+        )}
+
+        <button
+          type="button"
+          onClick={handleSubmit(onSubmit)}
+          disabled={formState.isSubmitting}
         >
-          Learn React
-        </a>
-      </header>
+          Sign Up
+        </button>
+      </form>
     </div>
   );
+}
+
+function App() {
+  return <Form />;
 }
 
 export default App;
